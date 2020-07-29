ec8ee2ed7790e09cdbb56906a0a2e1af53aa3738
Peyala Samarasimha Reddy <samarasimhareddy369@gmail.com>
2020-07-29 14:38:09 +0530

    Add files via upload

---
+++ OLED_DHT22/oled_dht22.ino
@@ -0,0 +1,63 @@
+
+ 
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+
+#define dataPin D7
+#define OLED_RESET D5
+Adafruit_SSD1306 display(OLED_RESET);
+
+// Initialize DHT sensor
+DHT dht(dataPin, DHT22);
+
+void setup() {
+      pinMode(D6,OUTPUT);
+      pinMode(D8,OUTPUT);
+      digitalWrite(D6,HIGH);
+      digitalWrite(D8,LOW);
+  Wire.begin();
+  dht.begin();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);// initialize with the I2C addr 0x3C
+}
+
+void displayTempHumid(){
+  delay(2000);
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    display.clearDisplay(); // clearing the display
+    display.setTextColor(WHITE); //setting the color
+    display.setTextSize(1); //set the font size
+    display.setCursor(5,0); //set the cursor coordinates
+    display.print("Failed to read from DHT sensor!");
+    return;
+  }
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("Humidity: "); 
+  display.print(h);
+  display.print(" %\t");
+  display.setCursor(0,10);
+  display.print("Temperature: "); 
+  display.print(t);
+  display.print(" C"); 
+  display.setCursor(0,20);
+  display.print("Temperature: "); 
+  display.print(f);
+  display.print(" F"); 
+}
+void loop() {
+  displayTempHumid();
+  display.display();
+}
