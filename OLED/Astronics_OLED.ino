d21c6d541eac02abc98fc1622051eac78840cb79
Peyala Samarasimha Reddy <samarasimhareddy369@gmail.com>
2020-07-29 14:46:04 +0530

    Add files via upload

---
+++ OLED/Astronics_OLED.ino
@@ -0,0 +1,31 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET D5
+/* Object named display, of the class Adafruit_SSD1306 */
+Adafruit_SSD1306 display(OLED_RESET);
+
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+void setup()   {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); /* Initialize display with address 0x3C */
+  display.clearDisplay();  /* Clear display */
+  display.setTextSize(1);  /* Select font size of text. Increases with size of argument. */
+  display.setTextColor(WHITE);  /* Color of text*/
+}
+
+void loop() {
+  display.clearDisplay();  /* Clear display */
+  drawStr(20, 2, "AStronics 2.0");
+  drawStr(60, 12, "by");
+  drawStr(20, 22, "Team E-Club");
+  display.display();
+
+}
+void drawStr(uint8_t x, uint8_t y, char* str){
+  display.setCursor(x, y);  /* Set x,y coordinates */
+  display.println(str);
+}
